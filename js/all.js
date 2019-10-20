4ca6a37557ad02ce133bdbf10b943f58d67fe963
RR-Hu <long20031001@hotmail.com>
2019-10-20 17:46:45 +0800

    index

---
+++ js/all.js
@@ -0,0 +1,26 @@
+var all = document.getElementById("blur");
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("menu-icon");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+	modal.style.display = "block";
+	btn.classList.add('de-emphasized');
+	all.classList.add('de-emphasized');
+}								
+span.onclick = function(){
+	modal.style.display = "none";
+	btn.classList.remove('de-emphasized');
+	all.classList.remove('de-emphasized');		
+}	
+window.onclick = function(event){
+  if(event.target == modal){
+	modal.style.display = "none";
+	btn.classList.remove('de-emphasized');
+	all.classList.remove('de-emphasized');			
+	}
+}
+function tagFunction(){
+	modal.style.display = "none";
+	btn.classList.remove('de-emphasized');
+	all.classList.remove('de-emphasized');		
+}
